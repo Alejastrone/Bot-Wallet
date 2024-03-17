4b3ff612cfa617b394981f113af4de46c2aa8dab
Alejandro <66971756+Alejastrone@users.noreply.github.com>
2024-03-17 15:28:35 -0300

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,10 +1,10 @@
 # Api Key, y Api Secret
 wallet = input("Elija la cuenta = ines:1 o alex:2 = ")
 if wallet == "1":
-	user = "Ines"
-	API_KEY = "hq7tGRGwZlcRLu0zXF7XXUGizVBryEEOCKGXJ5qoKHNrmaw638WpAhyhaKNrMrp1"
-	API_SECRET = "f5vTDZH6UuajtVFBcO6MKgRfeFaYskLB8oNcxBssXmKohEpNDDdKmySCoxvqRBEt"
+	user = 
+	API_KEY = 
+	API_SECRET = 
 elif wallet == "2":
-	user = "Alejandro"
-	API_KEY = "E6HbUIdg62mb25OiTV6fKwBB0b1ONoE2JcdL1sWypminROlOwwsS4xeQhoK80B0Y"
-	API_SECRET = "KmiVfiu0HRLsxgLtNId0Ph8N4asiKrpSHcFdVgPOzBS9oIpcvcBqKCNPHJrB7256"
+	user = 
+	API_KEY = 
+	API_SECRET = 
