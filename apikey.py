0f031a71f76bcf8f29a17f1bb6e55237f95bbb67
Alejandro <66971756+Alejastrone@users.noreply.github.com>
2024-04-28 14:59:49 -0300

    Add files via upload

--- apikey.py
+++ apikey.py
@@ -1,10 +1,14 @@
 # Api Key, y Api Secret
-wallet = input("Elija la cuenta = :1 o :2 = ")
+wallet = input("Elija la cuenta = Master:1, Developer:2, Produccion:3 = ")
 if wallet == "1":
-	user = 
-	API_KEY = 
-	API_SECRET = 
+	user = "Master"
+	API_KEY = "hq7tGRGwZlcRLu0zXF7XXUGizVBryEEOCKGXJ5qoKHNrmaw638WpAhyhaKNrMrp1"
+	API_SECRET = "f5vTDZH6UuajtVFBcO6MKgRfeFaYskLB8oNcxBssXmKohEpNDDdKmySCoxvqRBEt"
 elif wallet == "2":
-	user = 
-	API_KEY = 
-	API_SECRET = 
+	user = "Developer"
+	API_KEY = "E6HbUIdg62mb25OiTV6fKwBB0b1ONoE2JcdL1sWypminROlOwwsS4xeQhoK80B0Y"
+	API_SECRET = "KmiVfiu0HRLsxgLtNId0Ph8N4asiKrpSHcFdVgPOzBS9oIpcvcBqKCNPHJrB7256"
+elif wallet == "3":
+	user = "Produccion"
+	API_KEY = "tlvXngay5ZFjgQcO6HEMcPszAM9JGY8MGBlZAZnrNEJ6FAOThjTvNTHofv7WTvuR"
+	API_SECRET = "79hWOQzrjQE1s2PUUOw8HgjZ6Qdc9gXnUGt1vbiUpSqF4YHuHq9i5dHaJJ5cUa0B"	
